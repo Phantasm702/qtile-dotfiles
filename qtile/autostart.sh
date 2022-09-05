3103496d809cc81499cd222ddfc60fb06277bc62
Phantasm702 <jacobjean999@gmail.com>
2022-09-05 20:24:26 +0530

    add wallpaper

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -2,7 +2,7 @@
 
 picom --experimental-backends --backend glx &    # Compositor
 wal -R &    # Pywal
-nitrogen --restore &    # Wallpaper setter
+nitrogen --set-auto background.jpg &    # Wallpaper setter
 export LC_ALL=en_US.UTF-8
 optimus-manager-qt &    # Nvidia optimus manager systray icon
 ${HOME}/.config/qtile/scripts/update_checker.py & # Run check updates every 10 minutes
