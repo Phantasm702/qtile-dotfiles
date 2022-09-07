09f61003e630963e55c520cfc12466c0e36f379e
Phantasm702 <jacobjean999@gmail.com>
2022-09-07 11:46:20 +0530

    Remove jamesdsp from autostart.sh and set deault vol to 10

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -6,5 +6,4 @@ nitrogen --set-auto background.jpg &    # Wallpaper setter
 export LC_ALL=en_US.UTF-8
 optimus-manager-qt &    # Nvidia optimus manager systray icon
 ${HOME}/.config/qtile/scripts/update_checker.py & # Run check updates every 10 minutes
-jamesdsp -t &
-amixer -q sset Master 15% &
+amixer -q sset Master 10% &
